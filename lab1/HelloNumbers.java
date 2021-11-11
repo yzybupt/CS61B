16948e61de0e31ff6d70c01e810fb0c9b791ba6d
yinziyuan <yinziyuan6@outlook.com>
2021-11-11 21:28:23 +0800

    completed first part of lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,10 +1,10 @@
 public class HelloNumbers{
 	public static void main(String [] args){
 		int i=0;
-		int sum=0;
+		int su=0;
 		while(i<10){
-			sum=sum+i;
-			System.out.println(" "+sum);
+			su=su+i;
+			System.out.println(" "+su);
 		}
 
 	}
