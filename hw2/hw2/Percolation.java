0bc8f7b4998cc736e164e53a688d76797f65cfce
yinziyuan <yinziyuan6@outlook.com>
2022-01-28 22:55:04 +0800

    percolation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,92 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int size;
+    private int[][] grid;
+    private int number;
+    private WeightedQuickUnionUF unions;
 
+
+    private boolean checkBoarder (int x, int y) {
+        if (x < 0 || x > this.size - 1) {
+            return false;
+        } else if (y < 0 || y > this.size -1) {
+            return false;
+        }
+        return true;
+    }
+
+    private void connects (int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            int row = x + (int) Math.pow(-1, i);
+            int col = y + (int) Math.pow(-1, i + 1);
+            if(checkBoarder(row, col) && this.grid[row][col] == 1) {
+                unions.union(xyTo1d(x,y),xyTo1d(row,col));
+            }
+        }
+    }
+
+    private int xyTo1d(int x , int y) {
+
+        return x * this.size + y;
+    }
+
+    private void clear(int[][] grid) {
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                grid[i][j] = 0;
+            }
+        }
+    }
+
+    public Percolation(int N) { // create N-by-N grid, with all sites initially blocked
+        this.unions = new WeightedQuickUnionUF(N * N);
+        this.number = 0;
+        this.grid = new int[N][N];
+        this.size = N;
+        clear(this.grid);
+    }
+
+
+    public void open(int row, int col) { // open the site (row, col) if it is not open already
+        if (!isOpen(row,col)) {
+            this.grid[row][col] = 1;
+            this.number++;
+            connects(row,col);
+        }
+    }
+
+    public boolean isOpen(int row, int col) { // is the site (row, col) open?
+        return this.grid[row][col] == 1;
+    }
+
+    public boolean isFull(int row, int col) { // is the site (row, col) full?
+        for (int i = 0; i < this.size; i++) {
+            if (unions.connected(i, xyTo1d(row, col))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() { // number of open sites
+        return this.number;
+    }
+
+    public boolean percolates() { // does the system percolate?
+        for(int i = 0; i < this.size; i++) {
+            for (int j = 0; i < this.size; j++) {
+                if (unions.connected(i,xyTo1d(this.size - 1, j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) { // use for unit testing (not required)
+
+    }
 }
+
+
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,55 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
 
+public class PercolationStats {
+    private double[] results;
+    private int T;
+
+    private double experiment(int N, PercolationFactory pf) {
+        Percolation grid = pf.make(N);
+        while (!grid.percolates()) {
+            grid.open(StdRandom.uniform(N),StdRandom.uniform(N));
+        }
+        return (double) grid.numberOfOpenSites() / (N * N);
+    }
+
+    public PercolationStats(int N, int T, PercolationFactory pf)  { // perform T independent experiments on an N-by-N grid
+        this.T = T;
+        this.results = new double[T];
+        for (int i = 0; i < N; i++) {
+            results[i] = experiment(N, pf);
+        }
+    }
+
+
+    public double mean() {  // sample mean of percolation threshold
+        double sum = 0.0;
+        for (double d : results) {
+            sum = sum + d;
+        }
+        return (double) sum / results.length;
+    }
+
+
+    public double stddev(){ // sample standard deviation of percolation threshold
+        double sum = 0.0;
+        for (double d : results) {
+            sum = sum + (d - mean()) * (d - mean()) ;
+        }
+        return (double) sum / (results.length - 1);
+    }
+
+    public double confidenceLow() { // low endpoint of 95% confidence interval
+        double low;
+        low = mean() - 1.96 * Math.pow(stddev(), 0.5) / Math.pow(this.T, 0.5);
+        return low;
+
+    }
+
+    public double confidenceHigh() { // high endpoint of 95% confidence interval
+        double high;
+        high = mean() + 1.96 * Math.pow(stddev(), 0.5) / Math.pow(this.T, 0.5);
+        return high;
+    }
 }
